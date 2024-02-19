83bc254560b60b639b86acc45dfa4c3aa91cf8a3
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-19 15:15:08 -0800

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,52 +4,15 @@ import csv
 import requests
 import sys
 
-def write_todos_to_csv(todos_of_user,filename="{}.csv".format(sys.argv[1])):
-    """Writes the given todos to a CSV file, handling flattening and headers."""
-
-    flattened_todos = []
-    for todo in todos_of_user:
-        # Flatten using loop (alternative: recursive flattening for deeper nesting)
-        flattened_todo = {}
-        for key1, value1 in todo.items():
-            if isinstance(value1, dict):
-                for key2, value2 in value1.items():
-                    flattened_todo[key2] = value2
-            else:
-                flattened_todo[key1] = value1
-
-        # Alternatively, flatten using chain.from_iterable (requires collections module):
-        # flattened_todo = {key: value for key, value in chain.from_iterable((todo.items(), *[item.items() for item in todo.values() if isinstance(item, dict)]))}
-
-        flattened_todos.append(flattened_todo)
-
-    with open(filename, "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=flattened_todos[0].keys(), quoting=csv.QUOTE_ALL)
-        writer.writerows(flattened_todos)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos").json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    todos_of_user = []
-    for todo in todos:
-        arr = {}
-        if todo.get('userId') == user["id"]:
-            arr["USER_ID"] = user["id"]
-            arr["USERNAME"] = user['username']
-            arr["TASK_COMPLETED_STATUS"] = todo["completed"]
-            arr["TASK_TITLE"] = todo["title"]
-            todos_of_user.append(arr)
-
-    write_todos_to_csv(todos_of_user)
-
-
-
-
-
-
-
-
-
-
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
