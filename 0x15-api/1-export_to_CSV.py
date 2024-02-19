da874841fdda11da0b35151c963bc52bf8f90bc3
mena-rizkalla <mena.rizkalla1@gmail.com>
2024-02-19 15:08:38 -0800

    document file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Export data in CSV file"""
 import sys
 import requests
 import csv
-"""Export data in CSV file"""
 
 def write_todos_to_csv(todos_of_user,filename="{}.csv".format(sys.argv[1])):
     """Writes the given todos to a CSV file, handling flattening and headers."""
